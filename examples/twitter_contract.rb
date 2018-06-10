42ea6f094f6f21de014de1a0a98e93769d94cb2c
Sergey Dolganov <sclinede@gmail.com>
2018-06-10 11:06:00 +0300

    Move contract to TwitterSearch class

--- examples/twitter_contract.rb
+++ examples/twitter_contract.rb
@@ -11,7 +11,9 @@ class TwitterSearch
 
   def call(search_query)
     @search_query = search_query
-    @response = client.search(search_query)
+    TwitterSearchContract.new.match!(search_query) do
+      @response = client.search(search_query)
+    end
   end
 
   def print_stats
@@ -45,7 +47,7 @@ class TwitterSearch
 end
 
 require_relative '../poro_contract.rb'
-class TwitterContract < POROContract
+class TwitterSearchContract < POROContract
   private
 
   def twitter_response
@@ -54,39 +56,51 @@ class TwitterContract < POROContract
 
   # Guarantees
 
-  def guarantee_hash_response
+  def guarantee_response_structure
     @meta[:response_type] = twitter_response.class.to_s
     return unless twitter_response.respond_to?(:to_hash)
-    @twitter_response_hash = twitter_response.to_hash
-    true
-  end
 
-  def guarantee_response_structure
-    @statuses = @twitter_response_hash[:statuses]&.select do |s|
-      !s.dig(:user, :id).nil?
-    end
-    @meta[:statuses_count_without_user_id] =
-      @twitter_response_hash[:statuses].count - @statuses.count
-    !@statuses.nil? && @meta[:statuses_count_without_user_id].zero?
+    twitter_response_hash = twitter_response.to_hash
+    @tweets = response_tweets(twitter_response_hash)
+
+    !@tweets.nil? &&
+      tweets_count_without_user_id(twitter_response_hash, @tweets).zero?
   end
 
   # Expectations
 
   def expect_empty_search
-    @statuses.empty?
+    @tweets.empty?
   end
 
   def expect_non_empty_search
-    users_without_screen_name =
-      @statuses.select { |s| s.dig(:user, :screen_name).nil? }.tap do |statuses|
-        @meta[:user_ids_without_screen_name] = statuses.map { |s| s.dig(:user, :id) }
-      end
-    users_without_followers_count =
-      @statuses.select { |s| s.dig(:user, :followers_count).nil? }.tap do |statuses|
-        @meta[:user_ids_without_followers_count] = statuses.map { |s| s.dig(:user, :id) }
-      end
+    users_without_name(@tweets).empty? &&
+      users_without_followers_data(@tweets).empty?
+  end
 
-    users_without_screen_name.empty? && users_without_followers_count.empty?
+  # Helpers
+
+  def response_tweets(response_hash)
+    response_hash[:statuses]&.select do |s|
+      !s.dig(:user, :id).nil?
+    end
+  end
+
+  def tweets_count_without_user_id(response_hash, tweets)
+    @meta[:tweets_count_without_user_id] =
+      response_hash[:statuses].count - tweets.count
+  end
+
+  def users_without_name(tweets)
+    tweets.select { |s| s.dig(:user, :screen_name).nil? }.tap do |statuses|
+      @meta[:user_ids_without_screen_name] = statuses.map { |s| s.dig(:user, :id) }
+    end
+  end
+
+  def users_without_followers_data(tweets)
+    tweets.select { |s| s.dig(:user, :followers_count).nil? }.tap do |statuses|
+      @meta[:user_ids_without_followers_count] = statuses.map { |s| s.dig(:user, :id) }
+    end
   end
 end
 
@@ -102,6 +116,6 @@ alias ask_credentials credentials
 
 def search(search_query, credentials = ask_credentials)
   twitter_search = TwitterSearch.new(credentials)
-  TwitterContract.new.match!(search_query) { twitter_search.call(search_query) }
+  twitter_search.call(search_query)
   twitter_search.print_stats
 end
